178aef6b2885281a174db84c117c26553b1daebf
cvalverde96 <cvalverde96@gmail.com>
2025-01-27 17:21:30 -0400

    subiendo el main con todo los tasks aplicado del task_1

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -16,4 +16,60 @@ const teacher: Teacher = {
     contract: false,
 };
 
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'Nick',
+    lastName: 'Fury',
+    location: 'New York',
+    fullTimeEmployee: true,
+    numberOfReports: 1000,
+};
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface{
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+
+
+}
 console.log(teacher);
+console.log(director1);
+
+const result = printTeacher('John', 'Doe');
+console.log(result);
+
+const student = new StudentClass("John", "Doe");
+console.log(student.workOnHomework());
+console.log(student.displayName());
